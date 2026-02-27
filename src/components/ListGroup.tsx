798e818ee202fd1ff413a95bfce4fe0976736801
Oykun Yanilmaz <yanilmaz.oykun@gmail.com>
2026-02-27 22:35:16 +0300

    conditional rendering

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,11 +1,30 @@
 // import { Fragment } from "react/jsx-runtime";
 
 function ListGroup() {
-  const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  items = [];
+
+//   const message = items.length === 0 ? <p>No item found</p> : null;
+
+//   const getMessage = () => {
+//     return items.length === 0 ? <p>No item found</p> : null;
+//   }
+  
+//   if (items.length === 0)
+//     return (
+//       <>
+//         <h1>List</h1>
+//         <p>No item found</p>
+//       </>
+//     );
 
   return (
     <>
       <h1>List</h1>
+      {/* {message} */}
+      {/* {getMessage()} */}
+      {/* {items.length === 0 ? <p>No item found</p> : null} */}
+      {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
         {items.map((item) => (
           <li key={item}>{item}</li>
