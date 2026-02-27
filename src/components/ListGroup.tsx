dfa0780fac5b47ebc2b654e664e538677e78d6c8
Oykun Yanilmaz <yanilmaz.oykun@gmail.com>
2026-02-27 22:45:58 +0300

    handling events

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,22 +1,26 @@
 // import { Fragment } from "react/jsx-runtime";
+import { MouseEvent } from "react";
 
 function ListGroup() {
   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-  items = [];
+  //   items = [];
 
-//   const message = items.length === 0 ? <p>No item found</p> : null;
+  //   const message = items.length === 0 ? <p>No item found</p> : null;
 
-//   const getMessage = () => {
-//     return items.length === 0 ? <p>No item found</p> : null;
-//   }
-  
-//   if (items.length === 0)
-//     return (
-//       <>
-//         <h1>List</h1>
-//         <p>No item found</p>
-//       </>
-//     );
+  //   const getMessage = () => {
+  //     return items.length === 0 ? <p>No item found</p> : null;
+  //   }
+
+  //   if (items.length === 0)
+  //     return (
+  //       <>
+  //         <h1>List</h1>
+  //         <p>No item found</p>
+  //       </>
+  //     );
+
+  // Event Handler
+  const handleClick = (event:MouseEvent) => console.log(event);
 
   return (
     <>
@@ -26,8 +30,15 @@ function ListGroup() {
       {/* {items.length === 0 ? <p>No item found</p> : null} */}
       {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li key={item}>{item}</li>
+        {items.map((item, index) => (
+          <li
+            className="list-group-item"
+            key={item}
+            // onClick={(event) => console.log(item, index, event)}
+            onClick={handleClick}
+          >
+            {item}
+          </li>
         ))}
       </ul>
     </>
