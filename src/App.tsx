ee5e502f1fef361d5294e32d104cb9b3ba16a485
Oykun Yanilmaz <yanilmaz.oykun@gmail.com>
2026-02-28 17:15:35 +0300

    Passing children

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
 
 function App() {
   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
@@ -10,6 +11,9 @@ function App() {
   return (
     <div>
       <ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem}/>
+      <Alert>
+        Hello <span>World</span>
+      </Alert>
     </div>
   );
 }
