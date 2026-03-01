2751a67ac3474ffc0452c4d1293e2f906e1db0fa
Oykun Yanilmaz <yanilmaz.oykun@gmail.com>
2026-03-01 16:41:16 +0300

    Adding a new Expense in expense-tracker

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 import ExpenseList from "./expense-tracker/components/ExpenseList";
 import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
 import ExpenseForm from "./expense-tracker/components/ExpenseForm";
-import categories from "./expense-tracker/categories";
+
 
 function App() {
   const [selectedCategory, setSelectedCategory] = useState("");
@@ -23,7 +23,7 @@ function App() {
     <div>
       {/* <Form></Form> */}
       <div className="mb-5">
-        <ExpenseForm />
+        <ExpenseForm onSubmit={expense => setExpenses([...expenses, {...expense, id: expenses.length + 1}])}/>
       </div>
       <div className="mb-3">
         <ExpenseFilter
