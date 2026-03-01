37477bdbba79c6fef7d49ae4e1ddbfa5eaa194cc
Oykun Yanilmaz <yanilmaz.oykun@gmail.com>
2026-03-01 16:12:34 +0300

    Building expense form

--- src/expense-tracker/components/ExpenseFilter.tsx
+++ src/expense-tracker/components/ExpenseFilter.tsx
@@ -1,3 +1,5 @@
+import { categories } from "../../App";
+
 interface Props {
     onSelectCategory: (category: string) => void;
 }
@@ -6,9 +8,10 @@ const ExpenseFilter = ({onSelectCategory}: Props) => {
   return (
     <select className="form-select" onChange={(event) => onSelectCategory(event.target.value)}>
         <option value="">All Categories</option>
-        <option value="Groceries">Groceries</option>
+        {/* <option value="Groceries">Groceries</option>
         <option value="Utilities">Utilities</option>
-        <option value="Entertainment">Entertainment</option>
+        <option value="Entertainment">Entertainment</option> */}
+        {categories.map(category => <option key={category} value={category}>{category}</option>)}
     </select>
   )
 }
