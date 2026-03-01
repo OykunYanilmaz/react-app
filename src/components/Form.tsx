7e1b5dca89159a9e5e62cbb1a715616195f8c483
Oykun Yanilmaz <yanilmaz.oykun@gmail.com>
2026-03-01 15:01:50 +0300

    Disabling the submit button

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -7,7 +7,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 
 const schema = z.object({
     name: z.string().min(3, {message: 'Name must be at least 3 characters.'}),
-    age: z.number({invalid_type_error: 'Age field is required.'}).min(18, {message: 'Age must be at least 18.'})
+    age: z.number({error: 'Age field is required.'}).min(18, {message: 'Age must be at least 18.'})
 });
 
 type FormData = z.infer<typeof schema>;
@@ -38,7 +38,7 @@ const Form = () => {
   const {
     register,
     handleSubmit,
-    formState: { errors },
+    formState: { errors, isValid },
   } = useForm<FormData>({resolver: zodResolver(schema)});
   //   console.log(register('name'));
 
@@ -108,7 +108,7 @@ const Form = () => {
           <p className="text-danger">{errors.age.message}</p>
         )}
       </div>
-      <button className="btn btn-primary" type="submit">
+      <button disabled={!isValid} className="btn btn-primary" type="submit">
         Submit
       </button>
     </form>
