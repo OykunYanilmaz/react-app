3565fad2e74622cb0851324aad2a5564b1d2ad7a
Oykun Yanilmaz <yanilmaz.oykun@gmail.com>
2026-03-01 11:00:10 +0300

    Updating different kind of objects

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,59 @@
 import { useState } from "react";
+import Message from "./Message";
 
 function App() {
-  const [isVisible, setVisibility] = useState(false);
+  //   const [drink, setDrink] = useState({
+  //     title: "Americano",
+  //     price: 5,
+  //   });
+
+  //   const handleClick = () => {
+  //     // const newDrink = {
+  //     //     // title: drink.title,
+  //     //     ...drink,
+  //     //     price: 6
+  //     // }
+  //     // setDrink(newDrink);
+
+  //     setDrink({...drink, price:6});
+  //   };
+
+  // Another Example
+//   const [customer, setCustomer] = useState({
+//     name: "John",
+//     address: {
+//       city: "San Francisco",
+//       zipCode: 94111,
+//     },
+//   });
+
+//   const handleClick = () => {
+//     setCustomer({
+//       ...customer,
+//       address: { ...customer.address, zipCode: 94112 },
+//     });
+//   };
+
+  // Another Example : Array
+  const [tags, setTags] = useState(['happy', 'cheerful']);
 
   const handleClick = () => {
-    setVisibility(true);
-  }
+    // Add
+    setTags([...tags, 'exciting']);
+
+    // Remove
+    setTags(tags.filter(tag => tag !== 'happy'));
+
+    //Update
+    setTags(tags.map(tag => tag === 'happy' ? 'happiness' : tag));
+  };
 
   return (
     <div>
-      <button onClick={handleClick}>Show</button>
+      {/* {drink.price} */}
+      {/* {customer.address.zipCode} */}
+      {tags}
+      <button onClick={handleClick}>Click Me</button>
     </div>
   );
 }
--- src/Message.tsx
+++ src/Message.tsx
@@ -1,10 +1,9 @@
-// PascalCasing
-function Message() {
-    // JSX: Javascript XML
-    const name = 'Oykun';
-    if (name)
-        return <h1>Hello {name}</h1>;
-    return <h1>Hello World</h1>;
-}
+let count = 0;
+
+const Message = () => {
+  console.log('Message called', count);
+  count++;
+  return <div>Message {count}</div>;
+};
 
 export default Message;
