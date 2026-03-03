e5fd534c8607c1f499b8ce861f0afb58a530a9d0
Oykun Yanilmaz <yanilmaz.oykun@gmail.com>
2026-03-03 13:27:22 +0300

    Handling errors but this time working with async and await

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 import { use, useEffect, useState } from "react";
 
 interface User {
@@ -10,12 +10,31 @@ function App() {
   const [users, setUsers] = useState<User[]>([]);
   const [error, setError] = useState("");
 
+  // useEffect(() => {
+  //   // get -> promise -> response / err
+  //   axios
+  //     .get<User[]>("https://jsonplaceholder.typicode.com/xusers")
+  //     //  .then(response => console.log(response.data[0].name));
+  //     .then((response) => setUsers(response.data))
+  //     .catch((err) => setError(err.message));
+  // }, []);
+
   useEffect(() => {
-    axios
-      .get<User[]>("https://jsonplaceholder.typicode.com/xusers")
-      //  .then(response => console.log(response.data[0].name));
-      .then((response) => setUsers(response.data))
-      .catch((err) => setError(err.message));
+    const fetchUsers = async () => {
+      // get -> await promise -> response / err
+      
+      try {
+        const response = await axios
+          .get<User[]>("https://jsonplaceholder.typicode.com/xusers");
+        
+        setUsers(response.data);
+      }
+      catch (err) {
+        setError((err as AxiosError).message);
+      }
+    }
+
+    fetchUsers();
   }, []);
 
   return (
