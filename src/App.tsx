a2ee7df843a0f66a80c85fd09a544495e49dcc33
Oykun Yanilmaz <yanilmaz.oykun@gmail.com>
2026-03-03 13:40:47 +0300

    Cancelling a agetch request and showing a loading indicator

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import axios, { AxiosError } from "axios";
+import axios, { AxiosError, CanceledError } from "axios";
 import { use, useEffect, useState } from "react";
 
 interface User {
@@ -9,37 +9,56 @@ interface User {
 function App() {
   const [users, setUsers] = useState<User[]>([]);
   const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(false);
 
-  // useEffect(() => {
-  //   // get -> promise -> response / err
-  //   axios
-  //     .get<User[]>("https://jsonplaceholder.typicode.com/xusers")
-  //     //  .then(response => console.log(response.data[0].name));
-  //     .then((response) => setUsers(response.data))
-  //     .catch((err) => setError(err.message));
-  // }, []);
-
+  // Handling errors with chaining
   useEffect(() => {
-    const fetchUsers = async () => {
-      // get -> await promise -> response / err
-      
-      try {
-        const response = await axios
-          .get<User[]>("https://jsonplaceholder.typicode.com/xusers");
-        
-        setUsers(response.data);
-      }
-      catch (err) {
-        setError((err as AxiosError).message);
-      }
-    }
+    const controller = new AbortController();
 
-    fetchUsers();
+    setLoading(true);
+
+    // get -> promise -> response / err
+    axios
+      .get<User[]>("https://jsonplaceholder.typicode.com/users", {
+        signal: controller.signal,
+      })
+      //  .then(response => console.log(response.data[0].name));
+      .then((response) => {
+        setUsers(response.data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+        setLoading(false);
+      });
+
+    return () => controller.abort();
   }, []);
+
+  // Handling errors with async and await
+  // useEffect(() => {
+  //   const fetchUsers = async () => {
+  //     // get -> await promise -> response / err
+
+  //     try {
+  //       const response = await axios
+  //         .get<User[]>("https://jsonplaceholder.typicode.com/xusers");
+
+  //       setUsers(response.data);
+  //     }
+  //     catch (err) {
+  //       setError((err as AxiosError).message);
+  //     }
+  //   }
+
+  //   fetchUsers();
+  // }, []);
 
   return (
     <>
       {error && <p className="text-danger">{error}</p>}
+      {isLoading && <div className="spinner-border"></div>}
       <div>
         <ul>
           {users.map((user) => (
